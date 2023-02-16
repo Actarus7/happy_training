c0c4cc8f7cc4b0c7e5b51f2e3e3e7eed74fb88b0
Steff <steff.1@gmail.com>
2023-02-16 10:41:48 +0100

    "fix création template"

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ async function bootstrap() {
   app.use(helmet());
   // app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Happy Training')
+    .setDescription('The happy training API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('happy_training')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
