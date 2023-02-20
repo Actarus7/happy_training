b1d9abb2d0d33c04a61505c561f278fd719b1c91
Steff <steff.1@gmail.com>
2023-02-20 11:18:03 +0100

    feat add méthodes FavoriteTrainings

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Transform } from "class-transformer";
 import { IsBoolean, IsEmail, IsOptional, IsString, IsUrl, Length } from "class-validator";
 
 export class CreateUserDto {
