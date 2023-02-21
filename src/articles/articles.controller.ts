7f3ac49b29f0b707c4ef2db93b8f627e4388fd77
Pierrot <pierre.bournet@sncf.fr>
2023-02-21 14:32:06 +0100

    fix imports modules

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Logger, HttpStatus, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Logger, HttpStatus, Put, UseGuards } from '@nestjs/common';
 import { HttpException } from '@nestjs/common/exceptions';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateCommentDto } from 'src/comments/dto/create-comment.dto';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
@@ -13,9 +14,10 @@ export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) { }
 
 
-
+  @UseGuards(JwtAuthGuard)
   @Post()
-  async create(@Body() createArticleDto: CreateArticleDto) {Logger.log('create an article', 'ArticlesController');
+  async create(@Body() createArticleDto: CreateArticleDto) {
+    Logger.log('create an article', 'ArticlesController');
 
     const article = await this.articlesService.createArticle(createArticleDto);
 
@@ -28,7 +30,8 @@ export class ArticlesController {
 
 
   @Get()
-  async getAll() {Logger.log('get All articles', 'ArticlesController');
+  async getAll() {
+    Logger.log('get All articles', 'ArticlesController');
 
     return await this.articlesService.getArticles();
   };
@@ -37,8 +40,8 @@ export class ArticlesController {
 
   @Get(':articleId')
   async getOne(
-    @Param('articleId') articleId: number) 
-    {Logger.log('get One article', 'ArticlesController');
+    @Param('articleId') articleId: number) {
+    Logger.log('get One article', 'ArticlesController');
 
     const article = await this.articlesService.getOneArticle(articleId);
 
@@ -52,9 +55,9 @@ export class ArticlesController {
 
   @Patch(':articleId')
   async update(
-    @Param('articleId') articleId, 
-    @Body() updateArticleDto: UpdateArticleDto) 
-    {Logger.log('update an article','ArticlesController');
+    @Param('articleId') articleId,
+    @Body() updateArticleDto: UpdateArticleDto) {
+    Logger.log('update an article', 'ArticlesController');
 
     const article = await this.articlesService.updateArticle(articleId, updateArticleDto);
 
@@ -66,8 +69,8 @@ export class ArticlesController {
 
   @Patch('/published/:articleId')
   async publishedArticle(
-    @Param('articleId') articleId: number) 
-    {Logger.log('update an article','ArticlesController');
+    @Param('articleId') articleId: number) {
+    Logger.log('update an article', 'ArticlesController');
 
     const article = await this.articlesService.publishedArticle(articleId);
 
@@ -82,14 +85,14 @@ export class ArticlesController {
 
   @Delete(':articleId')
   async remove(
-    @Param('articleId') articleId) 
-    {Logger.log('remove an article', 'ArticlesController');
+    @Param('articleId') articleId) {
+    Logger.log('remove an article', 'ArticlesController');
 
     const article = await this.articlesService.removeArticle(articleId);
 
     if (article)
       return article;
-      
+
     throw new HttpException('Article not found', HttpStatus.NOT_FOUND);
   };
 
@@ -99,7 +102,7 @@ export class ArticlesController {
     if(comment)
     return comment;
   } */
-  
+
 };
 
 
